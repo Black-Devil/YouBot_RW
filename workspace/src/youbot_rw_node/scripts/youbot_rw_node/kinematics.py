fixed transformation to WristPoint under condition_angle; known bugs: inverse kinematic is wrong in calculation of theta_1, irgendein denkfehler! müssen wir mal per hand durchrechnen an nem Beispiel!

workspace/src/youbot_rw_node/scripts/youbot_rw_node/kinematics.py
@@ -60,10 +60,11 @@ class Kinematics:
 
 
     def get_transformation2wrist_point(self, alpha):
-        return np.matrix((  (cos(alpha),0,sin(alpha), 0),
-                    (0  ,   1   ,   0, 0),
-                    (-sin(alpha),   0,  cos(alpha), self.dh[5]['d']+self.dh[6]['d']),
-                    (0, 0, 0, 1)
+        dz =(self.dh[5]['d']+self.dh[6]['d'])
+        return np.matrix((  (cos(alpha),    0,      sin(alpha),     sin(alpha)*dz),
+                            (0  ,           1   ,   0,              0),
+                            (-sin(alpha),   0,      cos(alpha),     cos(alpha)*dz),
+                            (0,             0,      0,              1)
         ))
 
 
@@ -99,7 +100,7 @@ class Kinematics:
         return (trans*( np.matrix((0,0,0,1)).transpose() ))[:3]
 
 
-    def inverse_kin( self, point ):
+    def inverse_kin( self, point , condition_angle):
         """ calculate the inverse kinematics
 
         :param point on write plane, (x;y;z)
@@ -110,10 +111,12 @@ class Kinematics:
 
         wp= np.matrix(np.resize(point,4)).transpose()
         wp[3]=1                                                         # make homogenous coordinates
-        wp = self.get_transformation2wrist_point(45.0/180*np.pi) * wp   # offset calculate Wrist point under condition that Wrist is 45degree up on write plane
-        print "wpoint: [%.4f; %.4f; %.4f; %.4f]" % (wp[0],wp[1],wp[2], wp[3])
+        tran = self.get_transformation2wrist_point(math.radians(condition_angle))
+        #print "trafo2WP", tran
+        wp = self.get_transformation2wrist_point(math.radians(condition_angle)) * wp   # offset calculate Wrist point under condition that Wrist is 45degree up on write plane
+        #print "wpoint: [%.4f; %.4f; %.4f; %.4f]" % (wp[0],wp[1],wp[2], wp[3])
         wp_0=self.get_inv_transform(self.dh[0])*wp                    # transform wp into KS0
-        print "wp_0: [%.4f; %.4f; %.4f; %.4f]" % (wp_0[0],wp_0[1],wp_0[2], wp_0[3])
+        #print "wp_0: [%.4f; %.4f; %.4f; %.4f]" % (wp_0[0],wp_0[1],wp_0[2], wp_0[3])
         theta_0 = np.empty([2])
         theta_0[0]=arctan2(wp_0[1],wp_0[0])                 # turn robot arm into wrist point plane
 
@@ -121,20 +124,22 @@ class Kinematics:
             theta_0[1]=theta_0[0]+np.pi
         else:
             theta_0[1]=theta_0[0]-np.pi
-        print "theta_0: [%.4f; %.4f]" % (math.degrees(theta_0[0]),math.degrees(theta_0[1]))
+        #print "theta_0: [%.4f; %.4f]" % (math.degrees(theta_0[0]),math.degrees(theta_0[1]))
 
         wp_1 = np.array([self.get_inv_transform(self.dh[1],theta_0[0])*wp_0, self.get_inv_transform(self.dh[1],theta_0[1])*wp_0])       # numpy array of 2 points
-        print "wp_1_0: [%.4f; %.4f; %.4f; %.4f]" % (wp_1[0][0],wp_1[0][1],wp_1[0][2], wp_1[0][3])
-        print "wp_1_1: [%.4f; %.4f; %.4f; %.4f]" % (wp_1[1][0],wp_1[1][1],wp_1[1][2], wp_1[1][3])
+        #print "wp_1_0: [%.4f; %.4f; %.4f; %.4f]" % (wp_1[0][0],wp_1[0][1],wp_1[0][2], wp_1[0][3])
+        #print "wp_1_1: [%.4f; %.4f; %.4f; %.4f]" % (wp_1[1][0],wp_1[1][1],wp_1[1][2], wp_1[1][3])
 
         d_wp_1 = np.array(( np.sqrt((wp_1[0][0]**2 + wp_1[0][1]**2)),np.sqrt((wp_1[1][0]**2 + wp_1[1][1]**2)) ))    # array of 2 distances
-        print "d_wp_1: ", d_wp_1
+        #print "d_wp_1: ", d_wp_1
 
         s=np.array([arctan2(wp_1[0][1],wp_1[0][0]),arctan2(wp_1[1][1],wp_1[1][0])])                                 # array of 2 angles
-        #print  (self.dh[3]['a']**2-d_wp_1[0]**2-self.dh[2]['a']**2) / (-2*d_wp_1[0]*self.dh[2]['a'])
-        #print  (self.dh[3]['a']**2-d_wp_1[1]**2-self.dh[2]['a']**2) / (-2*d_wp_1[1]*self.dh[2]['a'])
+        #print "s: [%.4f; %.4f]" % (math.degrees(s[0]), math.degrees(s[1]))
+        #print "r arcos 0: ", (self.dh[3]['a']**2-d_wp_1[0]**2-self.dh[2]['a']**2) / (-2*d_wp_1[0]*self.dh[2]['a'])
+        #print "r arcos 1: ", (self.dh[3]['a']**2-d_wp_1[1]**2-self.dh[2]['a']**2) / (-2*d_wp_1[1]*self.dh[2]['a'])
         r=np.array([arccos( (self.dh[3]['a']**2-d_wp_1[0]**2-self.dh[2]['a']**2) / (-2*d_wp_1[0]*self.dh[2]['a']) ),
                     arccos( (self.dh[3]['a']**2-d_wp_1[1]**2-self.dh[2]['a']**2) / (-2*d_wp_1[1]*self.dh[2]['a']) )])              # array of 2 angles
+        #print "r: [%.4f; %.4f]" % (math.degrees(r[0]), math.degrees(r[1]))
 
         theta_1_0 = np.empty([2])                                                                                   #array of 2 angles
         theta_1_1 = np.empty([2])                                                                                   #array of 2 angles
@@ -144,8 +149,25 @@ class Kinematics:
         theta_1_1[0] =s[1]+r[1]
         theta_1_1[1] =s[1]-r[1]
 
+        # handle wrong turning direction--> make robot turn the shortest direction
+        for i in range(0,len(theta_1_0)):
+            if theta_1_0[i] < -np.pi:
+                theta_1_0[i] += np.pi*2
+            elif theta_1_0[i] > np.pi:
+                theta_1_0[i] -= np.pi*2
+        for i in range(0,len(theta_1_1)):
+            if theta_1_1[i] < -np.pi:
+                theta_1_1[i] += np.pi*2
+            elif theta_1_1[i] > np.pi:
+                theta_1_1[i] -= np.pi*2
+
+        #print "theta_1_0: [%.4f; %.4f]" % (math.degrees(theta_1_0[0]), math.degrees(theta_1_0[1]))
+        #print "theta_1_1: [%.4f; %.4f]" % (math.degrees(theta_1_1[0]), math.degrees(theta_1_1[1]))
+
         wp_2=  np.array([self.get_inv_transform(self.dh[2],theta_1_0[0])*wp_1[0], self.get_inv_transform(self.dh[2],theta_1_0[1])*wp_1[0] ,     # numpy array of 4 points
                          self.get_inv_transform(self.dh[2],theta_1_1[0])*wp_1[1], self.get_inv_transform(self.dh[2],theta_1_1[1])*wp_1[1]])
+
+        #print "wp_2: ", wp_2
 
         theta_2_0 = np.empty([2])                                                                                   #array of 2 angles
         theta_2_1 = np.empty([2])                                                                                   #array of 2 angles
@@ -155,6 +177,8 @@ class Kinematics:
         theta_2_1[0] = arctan2(wp_2[2][1], wp_2[2][0])
         theta_2_1[1] = arctan2(wp_2[3][1], wp_2[3][0])
 
+        #print "theta_2_0: [%.4f; %.4f]" % (math.degrees(theta_2_0[0]), math.degrees(theta_2_0[1]))
+        #print "theta_2_1: [%.4f; %.4f]" % (math.degrees(theta_2_1[0]), math.degrees(theta_2_1[1]))
 
         wp_3=  np.array([self.get_inv_transform(self.dh[3],theta_2_0[0])*wp_2[0], self.get_inv_transform(self.dh[3],theta_2_0[1])*wp_2[0] ,     # numpy array of 4 points
                          self.get_inv_transform(self.dh[3],theta_2_1[0])*wp_2[1], self.get_inv_transform(self.dh[3],theta_2_1[1])*wp_2[1]])
@@ -208,7 +232,14 @@ class Kinematics:
         :returns: todo
         :rtype: todo
         """
-        ik_solutions = self.inverse_kin(point)
+        #ik_solutions = self.inverse_kin(point, condition_angle)
+        condition_angle = np.array([ 45.0, 50.0, 55.0, 60.0, 65.0 , 70.0, 75.0, 80.0, 85.0, 90.0 ])
+        ik_solutions = list()
+        for i in range(0,len(condition_angle)):
+            print "condition: ", condition_angle[i]
+            tmpSol =  self.inverse_kin(point, condition_angle[i])
+            for k in tmpSol:
+                ik_solutions.append(k)
 
         valid_solutions = list()
         for i in ik_solutions: