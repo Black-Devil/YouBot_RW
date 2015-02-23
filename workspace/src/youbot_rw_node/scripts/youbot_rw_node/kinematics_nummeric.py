ff6e494a2547a47d49ac3bf0f2c67c8f71d20193
Julian Scholle <Julian.Scholle@googlemail.com>
2015-02-23 14:10:20 +0100

    imporve doku

--- workspace/src/youbot_rw_node/scripts/youbot_rw_node/kinematics_nummeric.py
+++ workspace/src/youbot_rw_node/scripts/youbot_rw_node/kinematics_nummeric.py
@@ -32,7 +32,9 @@ class Kinematics_num(Kinematics_base):
 
     def err_function(self, thetas):
         """ Error function used for optimisation
-        @param thetas Arm-Joint Angles in radians.
+
+        @param [in] thetas <b><i><c> [vector-4]: </c></i></b> Arm-Joint Angles in radians.
+        @return <b><i><c> [skalar]: </c></i></b> Error of thetas to point
         """
         new_theta = [thetas[0], thetas[1], thetas[2], thetas[3], 0]
         errors = self.direct_kin(new_theta) - self.destination_point
@@ -40,7 +42,8 @@ class Kinematics_num(Kinematics_base):
 
     def ist_rund(self, point):
         """ check point is around destination_point
-        @param point point to check
+        @param [in] point <b><i><c> [vector-3]: </c></i></b> point to check
+        @return <b><i><c> [bool]: </c></i></b> true if point near to destination_point
         """
         if abs(point[0] - self.destination_point[0]) > 0.03:
             return False
@@ -53,16 +56,17 @@ class Kinematics_num(Kinematics_base):
 
     def update_progresss(self, progress):
         """ updates Progressbar
-        @param progress percent of progress
+        @param [in] progress <b><i><c> [skalar]: </c></i></b> point percent of progress
         """
         sys.stdout.write('\r[{0}{1}] {2:3.2f}%'.format('#' * (int(progress / 2)), ' ' * ((100 / 2) - (int(progress / 2))), progress))
         sys.stdout.flush()
 
 
     def search_all_solutions(self, point, resolution):
-        """ solves the inverse kinematics problem
-        @param point point to reach
-        @param resolution Resolution of search tree in degree
+        """ solves the inverse kinematics problem (brute force)
+        @param [in] point <b><i><c> [vector-3]: </c></i></b> point to reach
+        @param [in] resolution <b><i><c> [skalar]: </c></i></b> search resolution in degree
+        @return <b><i><c> [list of vector-4]: </c></i></b> solutions found
         """
         resolution = math.fabs(resolution)
         self.destination_point = point
@@ -114,7 +118,8 @@ class Kinematics_num(Kinematics_base):
 
     def find_best_solution(self, solutions):
         """ finds and optimizes the best solution from a list of solutions
-        @param solutions list of solutions
+        @param [in] solutions <b><i><c> [list of vector-4]: </c></i></b> list of solutions
+        @return <b><i><c> [vector-4]: </c></i></b> best solution
         """
         best_sol = np.array(solutions.pop(0))
         min = self.err_function(best_sol)
@@ -138,9 +143,11 @@ class Kinematics_num(Kinematics_base):
 
 
     def step_to_point(self, point):
-        """ Moves the arm to Point
-        @param point point to reach
+        """ gets an solution to reach point if possible, uses optimizer or brute force if necessary
+        @param [in] point <b><i><c> [vector-3]: </c></i></b> point to reach
+        @return <b><i><c> [vector-4]: </c></i></b> joint states (solution)
         """
+
         # print "Distance from arm: ", math.sqrt((point[0]+0.35)**2+(point[1])**2)
         #if math.sqrt((point[0] + 0.35) ** 2 + (point[1]) ** 2) > 0.50:
         #    print "Point is not reachable"
@@ -177,7 +184,8 @@ class Kinematics_num(Kinematics_base):
 
     def checkSolution(self, solution):
         """ Checked solution on validity and fault tolerance
-        @param solution solution to check
+        @param [in] solution <b><i><c> [vector-4]: </c></i></b> solution to check
+        @return <b><i><c> [bool]: </c></i></b> true if solution is good
         """
         if self.err_function(solution) > 0.0001 or not self.isSolutionValid(solution):
             return False
@@ -188,8 +196,10 @@ class Kinematics_num(Kinematics_base):
         return 0
 
     def minimize(self, startPoint):
-        """ minimize the distanze to destination point
-        @param startPoint Point to start the optimisation
+        """ minimize the distance to destination point
+
+        @param [in] startPoint <b><i><c> [vector-4]: </c></i></b> Point to start the optimisation (Joint-Angles)
+        @return <b><i><c> [vector-5]: </c></i></b> hopefully a solution of the ik problem
         """
         erg = minimize(self.err_function, startPoint[0:4], method='SLSQP', options={'maxiter': 1e6, 'disp': False},
                        bounds=[(self.min_angles_[0], self.max_angles_[0]), (self.min_angles_[1], self.max_angles_[1]),
@@ -199,8 +209,8 @@ class Kinematics_num(Kinematics_base):
 
     def isSolutionValid(self, solution):
         """ checks the solution validity, (angle bounds)
-        @rtype : bool
-        @param solution solution to check
+        @param [in] solution <b><i><c> [vector-4]: </c></i></b> solution solution to check
+        @return <b><i><c> [bool]: </c></i></b> true if solution is valid
         """
         if len(solution) != 5:
             return False
@@ -216,9 +226,9 @@ class Kinematics_num(Kinematics_base):
 
     def isSolutionValid_single(self, solution, number):
         """ checks the solution validity for only one angle, (angle bounds)
-        @rtype : bool
-        @param solution solution to check
-        @param number Number of angle to check
+        @param [in] solution <b><i><c> [vector-4]: </c></i></b> solution to check
+        @param [in] number <b><i><c> [skalar]: </c></i></b> number of angle to check
+        @return <b><i><c> [bool]: </c></i></b> true if angle is OK
         """
         if solution < self.min_angles_[number] or solution > self.max_angles_[number]:
             return False
